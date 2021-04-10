81ad110fb0a4348cb4409c631e90796085a662e7
张雨帆 <zhangyufan@moqipobing.com>
2021-04-10 16:52:48 +0800

    fix(template): init

---
+++ src/app.vue
@@ -0,0 +1,30 @@
+<!--  -->
+<template>
+  <div class="gua">GUA22</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+/*
+---
+rootSize: 75
+---
+*/
+.gua {
+  font-size: 100px;
+}
+</style>
